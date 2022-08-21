062937fd2666e9a7ec1fb11c776a0e5e37d463e0
LORDNR <6250110009@psu.ac.th>
2022-08-21 23:25:22 +0700

    feat: add post

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,25 @@
 import Express, { Request, Response } from 'express'
 const { PrismaClient } = require('@prisma/client')
 
+
 const app = Express()
 const prisma = new PrismaClient()
 
+app.use(Express.json())
+
 app.get('/', async (req: Request, res: Response) => {
   const foods = await prisma.food.findMany()
+
+  res.json(foods)
+})
+
+app.post('/', async (req: Request, res: Response) => {
+  const foods = await prisma.food.create({
+    data: {
+      name: req.body.name,
+      price: Number(req.body.price)
+    }
+  })
 
   res.json(foods)
 })
